461aba101f6ef1a44b6a4faef13f79ea26dedec1
Arnab-pachal <ap.23cs8031@nitdgp.ac.in>
2025-09-04 15:02:23 +0530

    added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // ✅ use Vite default for Vercel
+    outDir: 'build', 
   },
   css: {
     postcss: './postcss.config.js',
   },
-  base: './', // ✅ ensures assets load correctly after deploy
-})
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,    
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,    
+  },
+  base: '/', 
+});
